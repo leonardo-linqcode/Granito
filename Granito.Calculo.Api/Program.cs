87a3013459ed9e97c30b26a097c0f884bde2d476
Leonardo Silva <leonardo.silva@autonomoz.com.br>
2023-01-05 00:33:31 -0300

    Habilitando Swagger em produção

--- Granito.Calculo.Api/Program.cs
+++ Granito.Calculo.Api/Program.cs
@@ -50,11 +50,11 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
+        //if (app.Environment.IsDevelopment())
+        //{
             app.UseSwagger();
             app.UseSwaggerUI();
-        }
+        //}
 
         app.UseExceptionHandler();
 
--- Granito.Taxas.Api/Program.cs
+++ Granito.Taxas.Api/Program.cs
@@ -20,11 +20,11 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
+        //if (app.Environment.IsDevelopment())
+        //{
             app.UseSwagger();
             app.UseSwaggerUI();
-        }
+        //}
 
         app.UseExceptionHandler();
 
