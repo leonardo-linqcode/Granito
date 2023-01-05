287c40c8487bcfa90a3455eec5ea44ed486b0277
Leonardo Silva <leonardo.silva@autonomoz.com.br>
2023-01-04 23:19:21 -0300

    Ajuste de arquivo DockerFile

--- Granito.Taxas.Api/Program.cs
+++ Granito.Taxas.Api/Program.cs
@@ -1,44 +1,41 @@
 using MediatR;
 using System.Reflection;
 
-namespace Granito.Taxas.Api
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddProblemDetails();
+
+        builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddProblemDetails();
-
-            builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseExceptionHandler();
-
-            app.UseStatusCodePages();
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseExceptionHandler();
+
+        app.UseStatusCodePages();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
