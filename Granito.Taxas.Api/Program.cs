4eb6085efc9689d7fd4e6f23b8baf2d67352a52f
Leonardo Silva <leonardo.silva@autonomoz.com.br>
2023-01-04 23:03:55 -0300

    Ajuste no arquivo program.cs para funcionar Docker Build

--- Granito.Taxas.Api/Program.cs
+++ Granito.Taxas.Api/Program.cs
@@ -1,40 +1,44 @@
 using MediatR;
 using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddProblemDetails();
-
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseExceptionHandler();
-
-app.UseStatusCodePages();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
 namespace Granito.Taxas.Api
 {
-    public partial class Program { }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddProblemDetails();
+
+            builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseExceptionHandler();
+
+            app.UseStatusCodePages();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
